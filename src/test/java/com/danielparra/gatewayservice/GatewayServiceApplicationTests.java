dab15d16e0de163e8885b9fd429053befaf1519b
Daniel Parra <dani.parra.90@gmail.com>
2019-10-27 10:08:13 +0100

    Initial project

---
+++ src/test/java/com/danielparra/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielparra.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
